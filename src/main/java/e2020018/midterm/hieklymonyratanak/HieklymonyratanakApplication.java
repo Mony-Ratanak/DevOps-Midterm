4003754213e6ee970bd648f398b7ead86505389b
Mony Ratanak <hieklymonyratanak@gmail.com>
2024-03-29 13:55:52 +0700

    Push springboot grandle to new branch

---
+++ src/main/java/e2020018/midterm/hieklymonyratanak/HieklymonyratanakApplication.java
@@ -0,0 +1,13 @@
+package e2020018.midterm.hieklymonyratanak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HieklymonyratanakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HieklymonyratanakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/e2020018/midterm/hieklymonyratanak/HieklymonyratanakApplicationTests.java
@@ -0,0 +1,13 @@
+package e2020018.midterm.hieklymonyratanak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HieklymonyratanakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
